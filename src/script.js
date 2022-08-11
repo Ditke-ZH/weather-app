f9d6988c39200eb2f5004b1859e9f813b2e820a1
Ditke-ZH <edit.goczan@gmail.com>
2022-08-11 16:51:27 +0200

    First upload of files

---
+++ src/script.js
@@ -0,0 +1,101 @@
+function displayWeatherCondition(response) {
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = `${temperature}°C`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "9170c7a6ca4c9d0527cdae0958a1a497";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let searchButton = document.querySelector("#search-icon");
+searchButton.addEventListener("click", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "9170c7a6ca4c9d0527cdae0958a1a497";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentTemperatureButton = document.querySelector("#location-icon");
+currentTemperatureButton.addEventListener("click", getCurrentLocation);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = 82;
+}
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertToFahrenheit);
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 28;
+}
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", convertToCelsius);
+
+let now = new Date();
+let date = now.getDate();
+let hours = String(now.getHours()).padStart(2, "0");
+let minutes = String(now.getMinutes()).padStart(2, "0");
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Okt",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${day}, ${date}. ${month} ${year}`;
+
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+searchCity("Zürich");
